f569a9be5b1cfe5f7c7849c22f41bfb0c8ee102f
Damian <darudami@gmail.com>
2023-08-10 16:33:51 -0400

    update kde install script, execute permission and removed unused files

--- kde.sh
+++ kde.sh
@@ -3,15 +3,15 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Switzerland -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector -c US -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
-makepkg -si --noconfirm
+#git clone https://aur.archlinux.org/pikaur.git
+#cd pikaur/
+#makepkg -si --noconfirm
 
 #pikaur -S --noconfirm system76-power
 #sudo systemctl enable --now system76-power
@@ -20,9 +20,12 @@ makepkg -si --noconfirm
 #sudo systemctl enable --now auto-cpufreq
 
 
-sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder obs-studio vlc papirus-icon-theme kdenlive materia-kde
+sudo pacman -S --noconfirm sddm plasma papirus-icon-theme materia-kde
+# sudo pacman -S --noconfirm --needed kde-applications
 
-sudo flatpak install -y spotify
+# sudo pacman -S flatpak
+
+# sudo flatpak install -y spotify
 
 sudo systemctl enable sddm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
